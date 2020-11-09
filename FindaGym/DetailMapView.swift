a8c53b25f238a4a62771bcfc4c9dbdf25ec7f8be
Booysenberry <brad.booysen@gmail.com>
2020-11-09 15:05:50 +1300

    Remove ad code

--- FindaGym/DetailMapView.swift
+++ FindaGym/DetailMapView.swift
@@ -8,13 +8,11 @@
 
 import UIKit
 import MapKit
-import GoogleMobileAds
 
-class DetailMapView: UIViewController, CLLocationManagerDelegate, GADBannerViewDelegate {
+class DetailMapView: UIViewController, CLLocationManagerDelegate {
     
     let locationManager = CLLocationManager()
     var mapRegion = MKCoordinateRegion()
-    let bannerView = GADBannerView(adSize: kGADAdSizeSmartBannerPortrait)
     
     @IBOutlet weak var mapView: MKMapView!
     
@@ -36,66 +34,7 @@ class DetailMapView: UIViewController, CLLocationManagerDelegate, GADBannerViewD
             placeAnnotation.title = place?.name
             mapView.addAnnotation(placeAnnotation)
         }
-        
-//        // Banner ad setup
-//        addBannerViewToView(bannerView)
-//        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
-//        bannerView.rootViewController = self
-//        bannerView.load(GADRequest())
-//        bannerView.delegate = self
     }
-    
-//    // Add banner to view
-//    func addBannerViewToView(_ bannerView: GADBannerView) {
-//        bannerView.translatesAutoresizingMaskIntoConstraints = false
-//        view.addSubview(bannerView)
-//        if #available(iOS 11.0, *) {
-//            // In iOS 11, we need to constrain the view to the safe area.
-//            positionBannerViewFullWidthAtBottomOfSafeArea(bannerView)
-//        }
-//        else {
-//            // In lower iOS versions, safe area is not available so we use
-//            // bottom layout guide and view edges.
-//            positionBannerViewFullWidthAtBottomOfView(bannerView)
-//        }
-//    }
-    
-//    // MARK: - banner ad view positioning
-//    @available (iOS 11, *)
-//    func positionBannerViewFullWidthAtBottomOfSafeArea(_ bannerView: UIView) {
-//        // Position the banner. Stick it to the bottom of the Safe Area.
-//        // Make it constrained to the edges of the safe area.
-//        let guide = view.safeAreaLayoutGuide
-//        NSLayoutConstraint.activate([
-//            guide.leftAnchor.constraint(equalTo: bannerView.leftAnchor),
-//            guide.rightAnchor.constraint(equalTo: bannerView.rightAnchor),
-//            guide.bottomAnchor.constraint(equalTo: bannerView.bottomAnchor)
-//        ])
-//    }
-    
-//    func positionBannerViewFullWidthAtBottomOfView(_ bannerView: UIView) {
-//        view.addConstraint(NSLayoutConstraint(item: bannerView,
-//                                              attribute: .leading,
-//                                              relatedBy: .equal,
-//                                              toItem: view,
-//                                              attribute: .leading,
-//                                              multiplier: 1,
-//                                              constant: 0))
-//        view.addConstraint(NSLayoutConstraint(item: bannerView,
-//                                              attribute: .trailing,
-//                                              relatedBy: .equal,
-//                                              toItem: view,
-//                                              attribute: .trailing,
-//                                              multiplier: 1,
-//                                              constant: 0))
-//        view.addConstraint(NSLayoutConstraint(item: bannerView,
-//                                              attribute: .bottom,
-//                                              relatedBy: .equal,
-//                                              toItem: view.safeAreaLayoutGuide.bottomAnchor,
-//                                              attribute: .top,
-//                                              multiplier: 1,
-//                                              constant: 0))
-//    }
     
     func setMapRegion() {
         if let placeLocation = currentPlace?.placemark.coordinate {
@@ -115,41 +54,6 @@ class DetailMapView: UIViewController, CLLocationManagerDelegate, GADBannerViewD
                 }
             }
         }
-    }
-    
-    // MARK: - Banner events (optional)
-    
-    // Tells the delegate an ad request loaded an ad.
-    func adViewDidReceiveAd(_ bannerView: GADBannerView) {
-        print("adViewDidReceiveAd")
-    }
-    
-    // Tells the delegate an ad request failed.
-    func adView(_ bannerView: GADBannerView,
-                didFailToReceiveAdWithError error: GADRequestError) {
-        print("adView:didFailToReceiveAdWithError: \(error.localizedDescription)")
-    }
-    
-    // Tells the delegate that a full-screen view will be presented in response
-    // to the user clicking on an ad.
-    func adViewWillPresentScreen(_ bannerView: GADBannerView) {
-        print("adViewWillPresentScreen")
-    }
-    
-    // Tells the delegate that the full-screen view will be dismissed.
-    func adViewWillDismissScreen(_ bannerView: GADBannerView) {
-        print("adViewWillDismissScreen")
-    }
-    
-    // Tells the delegate that the full-screen view has been dismissed.
-    func adViewDidDismissScreen(_ bannerView: GADBannerView) {
-        print("adViewDidDismissScreen")
-    }
-    
-    // Tells the delegate that a user click will open another app (such as
-    // the App Store), backgrounding the current app.
-    func adViewWillLeaveApplication(_ bannerView: GADBannerView) {
-        print("adViewWillLeaveApplication")
     }
 }
 
--- FindaGym/PlacesMapView.swift
+++ FindaGym/PlacesMapView.swift
@@ -9,9 +9,8 @@
 import UIKit
 import CoreLocation
 import MapKit
-import GoogleMobileAds
 
-class PlacesMapView: UIViewController, CLLocationManagerDelegate, GADBannerViewDelegate {
+class PlacesMapView: UIViewController, CLLocationManagerDelegate {
     
     @IBOutlet weak var mapView: MKMapView!
     
@@ -21,7 +20,6 @@ class PlacesMapView: UIViewController, CLLocationManagerDelegate, GADBannerViewD
     var nearbyPlaces = [Place]()
     var fetchedData = [MKMapItem]()
     var embedVC: PlaceListTable?
-    let bannerView = GADBannerView(adSize: kGADAdSizeSmartBannerPortrait)
     
     override func viewDidLoad() {
         
@@ -29,66 +27,7 @@ class PlacesMapView: UIViewController, CLLocationManagerDelegate, GADBannerViewD
         mapView.delegate = self
         setMapRegion()
         initLocationManager()
-        
-//        // Banner ad setup
-//        addBannerViewToView(bannerView)
-//        bannerView.adUnitID = "ca-app-pub-3940256099942544/2934735716"
-//        bannerView.rootViewController = self
-//        bannerView.load(GADRequest())
-//        bannerView.delegate = self
     }
-    
-//    // Add banner to view
-//    func addBannerViewToView(_ bannerView: GADBannerView) {
-//        bannerView.translatesAutoresizingMaskIntoConstraints = false
-//        view.addSubview(bannerView)
-//        if #available(iOS 11.0, *) {
-//            // In iOS 11, we need to constrain the view to the safe area.
-//            positionBannerViewFullWidthAtBottomOfSafeArea(bannerView)
-//        }
-//        else {
-//            // In lower iOS versions, safe area is not available so we use
-//            // bottom layout guide and view edges.
-//            positionBannerViewFullWidthAtBottomOfView(bannerView)
-//        }
-//    }
-    
-//    // MARK: - banner ad view positioning
-//    @available (iOS 11, *)
-//    func positionBannerViewFullWidthAtBottomOfSafeArea(_ bannerView: UIView) {
-//        // Position the banner. Stick it to the bottom of the Safe Area.
-//        // Make it constrained to the edges of the safe area.
-//        let guide = view.safeAreaLayoutGuide
-//        NSLayoutConstraint.activate([
-//            guide.leftAnchor.constraint(equalTo: bannerView.leftAnchor),
-//            guide.rightAnchor.constraint(equalTo: bannerView.rightAnchor),
-//            guide.bottomAnchor.constraint(equalTo: bannerView.bottomAnchor)
-//        ])
-//    }
-//
-//    func positionBannerViewFullWidthAtBottomOfView(_ bannerView: UIView) {
-//        view.addConstraint(NSLayoutConstraint(item: bannerView,
-//                                              attribute: .leading,
-//                                              relatedBy: .equal,
-//                                              toItem: view,
-//                                              attribute: .leading,
-//                                              multiplier: 1,
-//                                              constant: 0))
-//        view.addConstraint(NSLayoutConstraint(item: bannerView,
-//                                              attribute: .trailing,
-//                                              relatedBy: .equal,
-//                                              toItem: view,
-//                                              attribute: .trailing,
-//                                              multiplier: 1,
-//                                              constant: 0))
-//        view.addConstraint(NSLayoutConstraint(item: bannerView,
-//                                              attribute: .bottom,
-//                                              relatedBy: .equal,
-//                                              toItem: bottomLayoutGuide,
-//                                              attribute: .top,
-//                                              multiplier: 1,
-//                                              constant: 0))
-//    }
     
     @IBAction func centreMapOnUserButtonTapped(_ sender: Any) {
         self.mapView.setUserTrackingMode( MKUserTrackingMode.follow, animated: true)
@@ -136,37 +75,6 @@ class PlacesMapView: UIViewController, CLLocationManagerDelegate, GADBannerViewD
             locationManager.startUpdatingLocation()
             mapView.showsUserLocation = true
         }
-    
-//    // Checks the authorisation status of location services
-//    func checkAuthorisationStatus(){
-//
-//        let status = CLLocationManager.authorizationStatus()
-//
-//        if (status == .denied || status == .restricted || !CLLocationManager.locationServicesEnabled()){
-//
-//            let alert = UIAlertController(title: "Need Authorization", message: "We cannot find nearby places if you don't authorize the app to use your location", preferredStyle: .alert)
-//            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-//            alert.addAction(UIAlertAction(title: "Settings", style: .default, handler: { _ in
-//                let url = URL(string: UIApplication.openSettingsURLString)!
-//                UIApplication.shared.open(url, options: [:], completionHandler: nil)
-//            }))
-//            self.present(alert, animated: true, completion: nil)
-//
-//            print("User denied location access")
-//
-//            return
-//
-//
-//        }
-//
-//        if (status == .notDetermined){
-//            locationManager.requestWhenInUseAuthorization()
-//
-//        }
-//
-//        locationManager.startUpdatingLocation()
-//        mapView.showsUserLocation = true
-//    }
     
     func setMapRegion() {
         if let userLocation = locationManager.location?.coordinate {
@@ -309,40 +217,5 @@ extension PlacesMapView: MKMapViewDelegate {
             view.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
         }
         return view
-    }
-    
-    // MARK: - Banner events (optional)
-    
-    // Tells the delegate an ad request loaded an ad.
-    func adViewDidReceiveAd(_ bannerView: GADBannerView) {
-        print("adViewDidReceiveAd")
-    }
-    
-    // Tells the delegate an ad request failed.
-    func adView(_ bannerView: GADBannerView,
-                didFailToReceiveAdWithError error: GADRequestError) {
-        print("adView:didFailToReceiveAdWithError: \(error.localizedDescription)")
-    }
-    
-    // Tells the delegate that a full-screen view will be presented in response
-    // to the user clicking on an ad.
-    func adViewWillPresentScreen(_ bannerView: GADBannerView) {
-        print("adViewWillPresentScreen")
-    }
-    
-    // Tells the delegate that the full-screen view will be dismissed.
-    func adViewWillDismissScreen(_ bannerView: GADBannerView) {
-        print("adViewWillDismissScreen")
-    }
-    
-    // Tells the delegate that the full-screen view has been dismissed.
-    func adViewDidDismissScreen(_ bannerView: GADBannerView) {
-        print("adViewDidDismissScreen")
-    }
-    
-    // Tells the delegate that a user click will open another app (such as
-    // the App Store), backgrounding the current app.
-    func adViewWillLeaveApplication(_ bannerView: GADBannerView) {
-        print("adViewWillLeaveApplication")
     }
 }
